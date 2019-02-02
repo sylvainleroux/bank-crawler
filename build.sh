4944ab71948deb161441935feeabea5643c220cf
Sylvain LE ROUX <sylvain.leroux@gmail.com>
2019-02-02 13:16:20 +0100

    Fix build

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 
 rm *.deb
 
-./node_modules/.bin/node-deb -- index.js cron.js cmbImport.js src start.sh 
+./node_modules/.bin/node-deb -- index.js cron.js run_cmb_extract.js src run_cmb_load.js start.sh
