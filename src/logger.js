d39884c8182b33baf6af82fbe414114569bc7956
Sylvain LE ROUX <sylvain.leroux@gmail.com>
2020-08-28 19:28:01 +0200

    Custom log format

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,13 @@
-const winston = require("winston");
-const transport = new winston.transports.Console();
-const logger = winston.createLogger({
+const { format, createLogger, transports } = require("winston");
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+const logger = createLogger({
   level: "info",
-  transports: [transport],
-  format: winston.format.simple(),
+  transports: [new transports.Console()],
+  format: combine(timestamp(), myFormat),
 });
 
 if (process.env.NODE_ENV !== "production") {
